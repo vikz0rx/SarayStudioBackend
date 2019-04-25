e0d437b2ac3324c32ba2eb2c8f740c63ac927ee4
Виктор Загоруйко <vikz0rx@192.168.1.3>
2019-04-25 21:09:19 +0500

    Error fix

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import *
 
 class MultipleImagePhotographsInline(admin.TabularInline):
