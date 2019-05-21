9eb3a8ad934fedd5e609895c070a88ad4a76d530
vikz0rx <vikz0rx@gmail.com>
2019-05-21 22:05:24 +0500

    Rules url

--- api_v0/urls.py
+++ api_v0/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 router = DefaultRouter()
 
+router.register(r'rules', RulesViewSet)
 router.register(r'news', NewsViewSet)
 router.register(r'stuff', StuffViewSet)
 router.register(r'photographs', PhotographsViewSet)
